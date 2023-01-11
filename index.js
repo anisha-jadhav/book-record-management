493867591543f93ca6b87dd7dcee88f8f4157b3e
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-01-12 03:09:30 +0530

    Created connection with mongoDb and added dotenv file and data

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
-// importing users file
-const { users } = require("./Data/users.json");
+const dotenv = require("dotenv");
 
 //importing routes
 const usersRouter = require("./routes/users");
 const booksRouter = require("./routes/books");
+const DbConnection = require("./databaseConnection");
+
+dotenv.config();
 
 const app = express();
+
+DbConnection();
 
 const port = 8081;
 
@@ -20,140 +24,6 @@ app.get("/", (req, res) => {
 
 app.use("/users", usersRouter);
 app.use("/books", booksRouter);
-
-/**
- * Route: /users
- * Method: GET
- * Description: Get all users
- * Access: Public
- * Parameters: none
- */
-app.get("/users", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: users,
-  });
-});
-
-/**
- * Route: /users/:id   //:id means id is variable here
- * Method: GET
- * Description: Get single user by id
- * Access: Public
- * Parameters: id
- */
-app.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users.find((each) => each.id === id);
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "User not found",
-    });
-  }
-  return res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
-
-/**
- * Route: /users
- * Method: POST
- * Description: Create a new user
- * Access: Public
- * Parameters: none
- */
-app.post("/users", (req, res) => {
-  const { id, name, surname, email, subscriptionType, subscriptionDate } =
-    req.body;
-
-  const user = users.find((each) => each.id === id);
-
-  if (user) {
-    return res.status(404).json({
-      success: false,
-      message: "User exists with this id",
-    });
-  }
-
-  users.push({
-    id,
-    name,
-    surname,
-    email,
-    subscriptionType,
-    subscriptionDate,
-  });
-  return res.status(200).json({
-    success: true,
-    data: users,
-  });
-});
-
-/**
- * Route: /users/:id   //:id means id is variable here
- * Method: PUT
- * Description: Update a user
- * Access: Public
- * Parameters: id
- */
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-
-  const user = users.find((each) => each.id === id);
-
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "User not found.",
-    });
-  }
-
-  const updatedUser = users.map((each) => {
-    if (each.id === id) {
-      return {
-        ...each,
-        ...data,
-      };
-    }
-    return each;
-  });
-
-  return res.status(200).json({
-    success: true,
-    data: updatedUser,
-  });
-});
-
-/**
- * Route: /users/:id
- * Method: DELETE
- * Description: Delete a user by id
- * Access: Public
- * Parameters: id
- */
-
-app.delete("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users.find((each) => each.id === id);
-
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "User not found.",
-    });
-  }
-
-  const index = users.indexOf(user);
-  users.splice(index, 1);
-
-  return res.status(202).json({
-    success: true,
-    data: users,
-  });
-});
 
 app.get("*", (req, res) => {
   res.status(404).json({
